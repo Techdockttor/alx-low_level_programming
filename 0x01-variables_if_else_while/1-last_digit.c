0defe4b4a21eb7e40fcfb3561906259c88f0c76c
Techdockttor <diogu.ugochukwu@gmail.com>
2023-08-12 02:37:19 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,11 +1,11 @@
-#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 /**
+ * main - Entry point
  *
- * main - prints out the last digit of a randomly generated number and whether it is greater than 5, less than 6, or 0.
+ * Description: Prints all possible combinations of two two-digit numbers.
  *
- * Return: Eachtime (0).
+ * Return: Always 0 (Successful)
  */
 int main(void)
 {
@@ -15,13 +15,13 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	m = n % 10;
+
 	if (m > 5)
 		printf("Last digit of %d is %d and is greater tha 5\n", n, m);
 	else if (m == 0)
 		printf("Last digit of %d is %d and is 0\n", n, m);
 	else if (m < 6 && m != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
-
 	printf("\n");
 
 	return (0);
