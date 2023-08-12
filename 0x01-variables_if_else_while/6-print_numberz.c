1e76def8d6e966a1a13153241b278911bc134433
Techdockttor <diogu.ugochukwu@gmail.com>
2023-08-12 02:58:34 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Eachtime 0 (Successful)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
