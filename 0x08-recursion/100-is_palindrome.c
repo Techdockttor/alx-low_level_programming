2ed161ff3f6a51b727d8cf0a0f4893115fcc1ed0
Techdockttor <diogu.ugochukwu@gmail.com>
2023-08-30 05:35:44 -0700

    0x08-recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,23 @@
+int check_palindrome(int str, int len, char *p)
+{
+	if (str >= len)
+		return (1);
+	else if (p[str] != p[len])
+		return (0);
+	else
+		return (check_palindrome(str + 1, len - 1, p));
+}
+
+/**
+ * is_palindrome - states if a string is a palindrome
+ * @s: string to check
+ *
+ * Return: 1 if palindrome, else 0
+ */
+int is_palindrome(char *s)
+{
+	int i;
+
+	i = check_strlen(s) - 1;
+	return (check_palindrome(0, i, s));
+}
