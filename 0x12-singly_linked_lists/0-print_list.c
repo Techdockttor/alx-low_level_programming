c690a5b4c194c7015797ed6aea26e1664e908c1e
Techdockttor <diogu.ugochukwu@gmail.com>
2023-09-21 06:00:54 -0700

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "roll.h"
+#include <stdio.h>
+
+/**
+ * print_list - Prints all the elements of a list_t list.
+ * @h: A pointer to the list_t list (start of the list).
+ *
+ * Return: The number of nodes in the list.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while (h != NULL)
+	{
+		switch (h->str == NULL)
+		{
+			case 1:
+				printf("[%d] %s\n", 0, "(nil)");
+				break;
+			case 0:
+				printf("[%d] %s\n", h->len, h->str);
+				break;
+		}
+
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
+
---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "roll.h"
+
+/**
+ * list_len - Counts the number of nodes in a linked list.
+ * @h: Pointer to the head of the linked list.
+ *
+ * Return: The number of nodes in the list.
+ */
+size_t list_len(const list_t *h)
+{
+	int nodes = 0;
+
+	while (h != NULL)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
+
