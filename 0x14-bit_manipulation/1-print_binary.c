e8ce8335c539e20c967a3c8f536baea1f985cbbe
Techdockttor <diogu.ugochukwu@gmail.com>
2023-09-29 01:10:16 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,16 +12,3 @@ void print_binary(unsigned long int n)
 
 	_putchar((n & 1) + '0');
 }
-
-/**
- * _putchar - Writes a character to stdout
- * @c: The character to write
- *
- * Return: On success, the number of characters written.
- *         On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
