e7a7a8fd0603108e596cdd6fd036713e30404085
Techdockttor <diogu.ugochukwu@gmail.com>
2023-09-28 23:38:00 -0700

    0x14-bit_manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_endianness - Checks the endianness of the system.
+ *
+ * Return:
+ * 0 if the system is big-endian.
+ * 1 if the system is little-endian.
+ */
+
+
+int get_endianness(void)
+{
+    char *kytePtr;
+    int balux = 1;
+
+    kytePtr = (char *)&balux;
+
+    return (*kytePtr);
+}
